7309ddf32c4c0f0bf220838dc7bfbd8ee5cec245
Marvin Méndez <marvinmendezdevs@gmail.com>
2025-10-06 13:01:41 -0600

    Add fetch and then using a ContextAPI Libr

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,51 @@ import Experience from "./components/Experience";
 import Project from "./components/Project";
 
 function App() {
-  const [cursor, setCursor] = useState({x: 0, y: 0});
+  const [cursor, setCursor] = useState({ x: 0, y: 0 });
   const [space, setSpace] = useState(1);
+  const [personalInfo, setPersonalInfo] = useState({});
 
   useEffect(() => {
-    const updatePosition = (e : MouseEvent) => {
-      setCursor({x: e.clientX, y: e.clientY})
-    }
+    const getPersonalInformation = async () => {
+      try {
+        const response = await fetch('https://portafolio-backend-w3a6.onrender.com/personal-information');
+        const result = await response.json();
 
+        if(result.status !== 'ok') throw Error('Error al descargar la información')
+        console.log(result.data);
+        setPersonalInfo(result);
+      } catch (error) {
+        console.error('Error al obtener la información personal:', error);
+      }
+    };
+
+    getPersonalInformation();
+
+    const updatePosition = (e: MouseEvent) => {
+      setCursor({ x: e.clientX, y: e.clientY });
+    };
     window.addEventListener('mousemove', updatePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updatePosition);
+    };
   }, []);
+
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto max-w-7xl">
-      <div className="cursor -z-10 fixed rounded-full bg-red-50 -translate-x-1/2 -translate-y-1/2" style={{left: cursor.x, top: cursor.y, height: '30vw', width: '30vw'}}></div>
+      <div className="cursor -z-10 fixed rounded-full bg-red-50 -translate-x-1/2 -translate-y-1/2" style={{ left: cursor.x, top: cursor.y, height: '30vw', width: '30vw' }}></div>
       <div>
         <Colum1
-          space={ space }
-          setSpace={ setSpace }
+          space={space}
+          setSpace={setSpace}
         />
       </div>
       <div className="md:flex flex-col overflow-y-auto py-10 md:h-screen">
         {
           space === 1 ? <About />
-          : space === 2 ? <Experience />
-          : <Project />
+            : space === 2 ? <Experience />
+              : <Project />
         }
       </div>
     </div>
