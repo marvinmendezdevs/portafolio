aa4b9adb71a30ffd36530a1e69ab2dc1120e9cc6
Marvin Mendez <marvinmendezdevs@gmail.com>
2025-10-07 06:30:22 -0600

    Agregando un contexto para el lenguaje

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Colum1 from './components/Colum1';
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Project from "./components/Project";
+import { langContext } from "./context/LangContext";
 
 function App() {
+  const lang = useContext(langContext);
   const [cursor, setCursor] = useState({ x: 0, y: 0 });
   const [space, setSpace] = useState(1);
-  const [personalInfo, setPersonalInfo] = useState({});
+  const [personalInfo, setPersonalInfo] = useState({
+    name: '',
+    title: '',
+  });
 
   useEffect(() => {
-    const getPersonalInformation = async () => {
-      try {
-        const response = await fetch('https://portafolio-backend-w3a6.onrender.com/personal-information');
-        const result = await response.json();
-
-        if(result.status !== 'ok') throw Error('Error al descargar la información')
-        console.log(result.data);
-        setPersonalInfo(result);
-      } catch (error) {
-        console.error('Error al obtener la información personal:', error);
-      }
-    };
-
-    getPersonalInformation();
-
     const updatePosition = (e: MouseEvent) => {
       setCursor({ x: e.clientX, y: e.clientY });
     };
@@ -36,11 +26,36 @@ function App() {
   }, []);
 
 
+  useEffect(() => {
+        const getPersonalInformation = async () => {
+      try {
+        const response = await fetch('https://portafolio-backend-w3a6.onrender.com/personal-information');
+        const result = await response.json();
+
+        if(result.status !== 'ok') throw Error('Error al descargar la información')
+        const { data } = result
+
+        console.log(data)
+        
+        setPersonalInfo({
+          name: data.name,
+          title: data.title[lang]
+        })
+      } catch (error) {
+        console.error('Error al obtener la información personal:', error);
+      }
+    };
+
+    getPersonalInformation();
+  }, [lang]);
+
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto max-w-7xl">
       <div className="cursor -z-10 fixed rounded-full bg-red-50 -translate-x-1/2 -translate-y-1/2" style={{ left: cursor.x, top: cursor.y, height: '30vw', width: '30vw' }}></div>
       <div>
         <Colum1
+          personalInfo={ personalInfo }
           space={space}
           setSpace={setSpace}
         />
--- src/components/Colum1.tsx
+++ src/components/Colum1.tsx
@@ -2,16 +2,22 @@ import {Dispatch, SetStateAction} from 'react';
 import Navegation from "./Navegation";
 import SocialMedia from "./SocialMedia";
 
+type PersonalInfoType = {
+  name: string
+  title: string
+}
+
 type Colum1Type = {
+  personalInfo: PersonalInfoType
   space: number;
   setSpace: Dispatch<SetStateAction<number>>;
 }
 
-function Colum1({space, setSpace} : Colum1Type) : React.JSX.Element {
+function Colum1({personalInfo, space, setSpace} : Colum1Type) : React.JSX.Element {
   return (
     <div className="md:h-screen flex flex-col pt-20 pb-10">
         <div className="flex-grow">
-          <h1 className="text-white font-black text-4xl">Marvin Méndez</h1>
+          <h1 className="text-white font-black text-4xl">{ personalInfo.name }</h1>
           <p className="text-white font-bold my-2">
             Software developer Engineer
           </p>
